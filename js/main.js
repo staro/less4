0fb5487858d451eae9dc9028252d62df55158e37
staro <staro.oleg@gmail.com>
2019-04-14 14:19:30 +0300

    First commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+'use strict';
+
+// console.log("arr" + " - object");
+// console.log(4 + " - object");
+
+/* let answer = +prompt("How old aree you?")
+
+console.log(typeof(answer)); */
+
+/* let incr = 10,
+    decr = 10;
+
+console.log(++incr);
+console.log(--decr); */
+
+// console.log(5%2); -- 1
+
+// console.log("2" == 2);  - true
+// console.log("2" === 2);  - false
+
+
+/* let isChecked = true,
+    isClose = false;
+
+console.log(isChecked && isClose); */
+
+let isChecked = true,
+    isClose = false;
+
+console.log(isChecked || isClose);
